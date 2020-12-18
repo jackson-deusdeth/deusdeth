4daa57b8f3029ac0c56a054922fd467a5ef9f336
jackson-deusdeth <jackson@ziromoda.com.br>
2020-12-18 14:53:33 -0300

    port

---
+++ Arrowfunction1.js
@@ -0,0 +1,19 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implicito
+console.log(dobro(Math.PI))
+
+let ola = function() {
+    return 'olá'
+}
+
+olá = () => 'olá'
+console.log(olá())
+
+
